78642ff2a126abe65e0b4454ca54ece82b642825
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 17:36:04 +0300

    Update tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -12,7 +12,7 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *FAISAL-MD*");
+        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *VENGEANCE-XMD*");
 
         // Create a new PDF document
         const doc = new PDFDocument();
@@ -25,11 +25,11 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             await conn.sendMessage(from, {
                 document: pdfData,
                 mimetype: 'application/pdf',
-                fileName: 'JawadTech.pdf',
+                fileName: 'VENGEANCE-XMD.pdf',
                 caption: `
 *📄 PDF created successully!*
 
-> © Created By FAISAL-MD 💜`
+> © Created By VENGEANCE-XMD 💜`
             }, { quoted: mek });
         });
 
