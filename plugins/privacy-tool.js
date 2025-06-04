2dbe2469a12be46856061c666054c668765fc8bf
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 16:46:09 +0300

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -41,14 +41,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/wtqlhp.jpg` }, // Replace with privacy-themed image if available
+                image: { url: `https://files.catbox.moe/k7cko8.jpg` }, // Replace with privacy-themed image if available
                 caption: privacyMenu,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '123456789123456789@newsletter',
+                        newsletterJid: '120363400583993139@newsletter',
                         newsletterName: "Privacy Settings",
                         serverMessageId: 143
                     }
