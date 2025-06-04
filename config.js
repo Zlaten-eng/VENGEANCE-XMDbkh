3a3f91ca0331662390b00950784880f5beb32ba4
Zlaten-eng <ronojepsat70@gmail.com>
2025-06-04 23:18:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254769677305",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254788409105",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Conway",
 // add bot owner name
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254769677305",
+DEV: process.env.DEV || "254788409105",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
