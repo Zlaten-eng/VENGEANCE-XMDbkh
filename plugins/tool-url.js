b05c776328c038fea9b1784c36288397eac816ea
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 17:44:35 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by FAISAL-MD 💜`
+      `> © Uploaded by VENGEANCE-XMD`
     );
 
   } catch (error) {
