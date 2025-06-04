b6ff2222560e9950217b68c2efd6b1f4ad626d2e
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 16:41:36 +0300

    Update pinterest-dl.js

--- plugins/pinterest-dl.js
+++ plugins/pinterest-dl.js
@@ -32,7 +32,7 @@ cmd({
         const videoUrl = media.find(item => item.type.includes('720p'))?.download_url || media[0].download_url;
 
         // Prepare the new message with the updated caption
-        const desc = `╭━━━〔 FAISAL-MD* 〕━━━┈⊷
+        const desc = `╭━━━〔 VENGEANCE-XMD* 〕━━━┈⊷
 ┃▸╭───────────
 ┃▸┃๏ *PINS DOWNLOADER*
 ┃▸└───────────···๏
@@ -41,7 +41,7 @@ cmd({
 ┇๏ *Title* - ${title}
 ┇๏ *Media Type* - ${media[0].type}
 ╰━━❑━⪼
-> *© Pᴏᴡᴇʀᴇᴅ Bʏ FAISAL-MD ♡*`;
+> *© Pᴏᴡᴇʀᴇᴅ Bʏ VENGEANCE-XMD*`;
 
         // Send the media (video or image) to the user
         if (videoUrl) {
