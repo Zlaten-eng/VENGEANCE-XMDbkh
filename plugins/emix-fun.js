717b694fdbccccde17434f01d2c10db2f0eb6555
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 14:38:36 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "FAISAL-MD",
+            author: "VENGEANCE-XMD",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
