831b5e3acb7f7c0704a680ed37eb4b47e8030bb4
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-04 16:01:10 +0300

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for FAISAL-MD updates...");
+        await reply("🔍 Checking for VENGEANCE-XMD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/Queen-anita/FAISAL-MD-watsapp-new-bot/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/VENGEANCE254/VENGEANCE-XMD /commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your FAISAL-MD bot is already up-to-date!");
+            return reply("✅ Your VENGEANCE-XMD bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating FAISAL-MD Bot...");
+        await reply("🚀 Updating VENGEANCE-XMD Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/Queen-anita/FAISAL-MD-watsapp-new-bot/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/VENGEANCE254/VENGEANCE-XMD /archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "FAISAL-MD-main");
+        const sourcePath = path.join(extractPath, "VENGEANCE-XMD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
